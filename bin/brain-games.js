177a35294d788aaba98d7c7e5ddf176709aa0431
Zhandos Zhanybek <zh.zhanybek@gmail.com>
2021-05-16 15:23:58 +0600

    adding eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import hello from "../src/cli.js";
+import hello from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 hello();

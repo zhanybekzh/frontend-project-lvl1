e686ec0f7bbc605266d583dd70ab117b33f9744a
Zhandos Zhanybek <zh.zhanybek@gmail.com>
2021-05-18 02:34:19 +0600

    delete eval func

--- src/games/CalcGame.js
+++ src/games/CalcGame.js
@@ -3,6 +3,10 @@ import startGame from '../index.js';
 import getRandomInt from '../util.js';
 
 const descr = 'What is the result of the expression?';
+const sum = (pair) => car(pair) + cdr(pair);
+const diff = (pair) => car(pair) - cdr(pair);
+const multiply = (pair) => car(pair) * cdr(pair);
+const functionsOfOperations = [sum, diff, multiply];
 const operations = ['+', '-', '*'];
 const getRound = () => {
   const firstRandomNumber = getRandomInt(1, 100);
@@ -10,7 +14,7 @@ const getRound = () => {
   const randomIndex = getRandomInt(0, 2);
   const pair = cons(firstRandomNumber, secondRandomNumber);
   const question = `${car(pair)} ${operations[randomIndex]} ${cdr(pair)}`;
-  const answer = String(eval(question));
+  const answer = String(functionsOfOperations[randomIndex](pair));
   return [answer, question];
 };
 

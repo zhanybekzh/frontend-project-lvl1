78c40a65d21efb2750a6538bf996bb58a4ae1435
Zhandos Zhanybek <zh.zhanybek@gmail.com>
2021-05-18 02:18:46 +0600

    calc.js

--- src/games/CalcGame.js
+++ src/games/CalcGame.js
@@ -1,12 +1,16 @@
+import { cons, car, cdr } from '@hexlet/pairs';
 import startGame from '../index.js';
 import getRandomInt from '../util.js';
 
 const descr = 'What is the result of the expression?';
-
+const operations = ['+', '-', '*'];
 const getRound = () => {
-  const randomNumber = getRandomInt(1, 100);
-  const answer = check(randomNumber);
-  const question = randomNumber;
+  const firstRandomNumber = getRandomInt(1, 100);
+  const secondRandomNumber = getRandomInt(1, 100);
+  const randomIndex = getRandomInt(0, 2);
+  const pair = cons(firstRandomNumber, secondRandomNumber);
+  const question = `${car(pair)} ${operations[randomIndex]} ${cdr(pair)}`;
+  const answer = String(eval(question));
   return [answer, question];
 };
 

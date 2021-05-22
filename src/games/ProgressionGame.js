75eff94795560a7e1f16fd2008539364352d9c04
Zhandos Zhanybek <zh.zhanybek@gmail.com>
2021-05-22 16:09:41 +0600

    adding new game progression

---
+++ src/games/ProgressionGame.js
@@ -0,0 +1,31 @@
+import startGame from '../index.js';
+import getRandomInt from '../util.js';
+
+const generateProgression = (length, step, firstElement) => {
+  const arr = [];
+  let k = firstElement;
+  for (let i = 0; i < length; i += 1) {
+    arr[i] = k;
+    k += step;
+  }
+  return arr;
+};
+const hideElement = (arr, index) => {
+  const answer = arr[index];
+  arr[index] = '..';
+  return answer;
+}
+const descr = 'Find the greatest common divisor of given numbers.';
+
+const getRound = () => {
+  const progressionLength = getRandomInt(5, 12);
+  const progressionStep = getRandomInt(1, 10);
+  const indexOfHiddenElement = getRandomInt(0, progressionLength - 1);
+  const firstElement = getRandomInt(1, 100);
+  const progressionArray = generateProgression(progressionLength, progressionStep, firstElement);
+  const answer = String(hideElement(progressionArray, indexOfHiddenElement));
+
+  const question = progressionArray.join(' ');
+  return [answer, question];
+};
+export default () => startGame(descr, getRound);

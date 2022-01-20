d2968f516ebc32eae80ac72148d8b41476fcb911
Zhandos <zh.zhanybek@gmail.com>
2022-01-21 01:29:44 +0600

    Вместо императивного подхода применил декларативное.

--- src/games/GcdGame.js
+++ src/games/GcdGame.js
@@ -1,18 +1,20 @@
 import startGame from '../index.js';
 import getRandomInt from '../util.js';
 
-const findMin = (num1, num2) => (num1 >= num2 ? num2 : num1);
-
 const gcd = (num1, num2) => {
   if (num1 === num2) {
     return num1;
   }
-  for (let i = findMin(num1, num2); i > 1; i -= 1) {
-    if (num1 % i === 0 && num2 % i === 0) {
-      return i;
+  const maxNum = (num1 < num2) ? num2 : num1;
+  const minNum = (num1 >= num2) ? num2 : num1;
+  const iter = (max, min) => {
+    if (max % min === 0) {
+      return min;
+    } else {
+      return iter(min, max % min);
     }
   }
-  return 1;
+  return iter(maxNum, minNum);
 };
 
 const description = 'Find the greatest common divisor of given numbers.';

14236baf24f62e919c1808de8b3980892fb7df6d
Zhandos <zh.zhanybek@gmail.com>
2022-01-20 20:24:50 +0600

    отменил возврат переменной в функции

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,5 @@ export default () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
+  return
 };

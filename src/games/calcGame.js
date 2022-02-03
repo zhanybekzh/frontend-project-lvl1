2c68f6c07a1a570af65f46f913875f72eca0a3ae
Zhandos <zh.zhanybek@gmail.com>
2022-02-03 23:34:09 +0600

    Убрал функцию cons

--- src/games/calcGame.js
+++ src/games/calcGame.js
@@ -1,16 +1,15 @@
-import { cons, car, cdr } from '@hexlet/pairs';
 import startGame from '../index.js';
 import {getRandomInt} from '../util.js';
 
 const description = 'What is the result of the expression?';
-const calculateFunction = (pair, operation) => {
+const calculateFunction = (firstNum, secondNum, operation) => {
   switch (operation) {
     case '+':
-      return car(pair) + cdr(pair);
+      return firstNum + secondNum;
     case '-':
-      return car(pair) - cdr(pair);
+      return firstNum - secondNum;
     case '*':
-      return car(pair) * cdr(pair);
+      return firstNum * secondNum;
     default:
       return null;
   }
@@ -20,9 +19,8 @@ const getRound = () => {
   const firstRandomNumber = getRandomInt(1, 100);
   const secondRandomNumber = getRandomInt(1, 100);
   const randomIndex = getRandomInt(0, operations.length - 1);
-  const pair = cons(firstRandomNumber, secondRandomNumber);
-  const question = `${car(pair)} ${operations[randomIndex]} ${cdr(pair)}`;
-  const answer = String(calculateFunction(pair, operations[randomIndex]));
+  const question = `${firstRandomNumber} ${operations[randomIndex]} ${secondRandomNumber}`;
+  const answer = String(calculateFunction(firstRandomNumber, secondRandomNumber, operations[randomIndex]));
   return [answer, question];
 };
 
